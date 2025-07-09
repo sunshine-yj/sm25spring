6977c3fce4c6db1699fb18298089eaf124602ff6
Kimyoujin <rladbwls3212003@naver.com>
2025-07-09 14:23:01 +0900

    0709 - (Day01 14:22)

---
+++ day01/src/main/java/eud/sm/day01/controller/MainController.java
@@ -0,0 +1,17 @@
+package eud.sm.day01.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@Slf4j
+public class MainController {
+
+    @RequestMapping("/")
+    public String main() {
+        log.info("start main ....");
+
+        return "index";
+    }
+}
---
+++ day01/src/main/webapp/views/index.jsp
@@ -0,0 +1,18 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: rladb
+  Date: 2025-07-09
+  Time: 오후 1:19
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+    <h1>Main Page</h1>
+    <h1>JMLEE</h1>
+    <h2></h2>
+</body>
+</html>
